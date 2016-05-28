0ef6c452e2c337bf83251464d8d0d207b0f780e2
Arun Sai <arunsai0310@gmail.com>
2016-05-28 14:17:01 +0000

    add styling to landing login register

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ var indexRoutes         = require("./routes/index"),
 
 
 // seedDB(); //seed the database
-mongoose.connect(process.env.DATABASEURL);
+var dburl = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v10";
+mongoose.connect(dburl);
+//mongoose.connect(process.env.DATABASEURL);
 // mongoose.connect("mongodb://localhost/yelp_camp_v10");
 // mongoose.connect("mongodb://arun:arun@ds017193.mlab.com:17193/arunsai0310");
 
